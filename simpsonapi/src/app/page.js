078b417b5f6993e8eb709a82a77701a6212532e4
Sebastián Peñaranda <sebaspenieto@gmail.com>
2025-11-23 14:58:31 -0500

    Paginación agregada, y detalles en la personalización

--- simpsonapi/src/app/page.js
+++ simpsonapi/src/app/page.js
@@ -8,15 +8,18 @@ const API_BASE_URL = "https://thesimpsonsapi.com/api/characters";
 export default function SimpsonsPage() {
   const [nombre, setNombre] = useState("");
   const [data, setData] = useState([]);
+  const [ant, setAnt] = useState("");
+  const [next, setNext] = useState("");
+  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    fetch(API_BASE_URL)
+   useEffect(() => {
+    fetch(`${API_BASE_URL}?page=${page}`)
       .then((response) => response.json())
       .then((responseJson) => {
         setData(responseJson.results);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [page]);
 
   const personajes_filtrados = data.filter((personaje) => 
     personaje.name.toLowerCase().includes(nombre.toLowerCase())
@@ -25,12 +28,33 @@ export default function SimpsonsPage() {
 
 
   return (
-    <div className="flex flex-col gap-4 bg-cyan-600 min-h-screen">
+    <div className="flex flex-col gap-4 bg-sky-100 min-h-screen">
+
+      {/* Paginación */}
+      <div className="flex justify-center gap-4  mt-8">
+        <button
+          onClick={() => setPage((prev) => prev - 1)}
+          disabled={page === 1}
+          className="bg-gray-700 text-white px-4 py-2 rounded disabled:opacity-50 ml-8 mr-60"
+        >
+          <div className="text-xl font-bold"> ◀ </div>
+        </button>
+
+        <div className="text-xl font-bold text-blue-900 p-2">Página {page}</div>
+
+        <button
+          onClick={() => setPage((prev) => prev + 1)}
+          disabled={page === 60}
+          className="bg-gray-700 text-white px-4 py-2 rounded disabled:opacity-50 mr-8 ml-60"
+        >
+         <div className="text-xl font-bold"> ▶ </div> 
+        </button>
+      </div>
 
       {/* Cuadro para solicitar el nombre del personaje: */}
-      <div className="my-2 flex gap-2 justify-center">
+      <div className="mb-8 mt-4 flex gap-2 justify-center">
         <input
-          className="bg-cyan-300 border-cyan-900 rounded-lg p-2 text-blue-900 border text-xl "
+          className="bg-blue-200 border-cyan-900 rounded-lg p-2 text-black border text-xl "
           type="text"
           value={nombre}
           onChange={(e) => {
@@ -40,7 +64,7 @@ export default function SimpsonsPage() {
       </div>  
 
       {/* Cuadro para mostrar cada personaje: */}
-      <div className="flex flex-wrap justify-center gap-4 p-4">
+      <div className="flex flex-wrap justify-center gap-8 p-4">
         {
           personajes_filtrados.map((personaje) => (
             <Character key = {personaje.id} data = {personaje}></Character>
--- simpsonapi/src/components/Character.jsx
+++ simpsonapi/src/components/Character.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function Character({ data }) {
   return (
-    <div className="flex flex-col items-center p-4 bg-white rounded-xl shadow">
-      <div className="text-xl font-bold mb-2">{data.name}</div>
+    <div className="flex flex-col items-center p-4 bg-blue-900 rounded-xl shadow">
+      <div className="text-xl font-bold mb-2 text-sky-100">{data.name}</div>
       <img
         className="h-36 w-36 rounded-lg"
         src={"https://cdn.thesimpsonsapi.com/500" + data.portrait_path}
